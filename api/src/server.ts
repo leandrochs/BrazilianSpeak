2ddad5302d5c84b6d9ae1146402e2fb960bc45b4
Hiago Briano <hiago.artist@hotmail.com>
2023-11-09 21:13:13 -0300

    rota de login criada

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express'
+import routerLogin from './routers/routerLogin';
 require('dotenv/config');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
+app.use('/login', routerLogin);
 app.get('/', (_req, res) => res.json({ message: 'active server' }));
 
 app.listen(+PORT, () => {
